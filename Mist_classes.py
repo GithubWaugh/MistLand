ef77cac4ef03a99cee7f468109d62df41461092d
GithubWaugh <78760614+GithubWaugh@users.noreply.github.com>
2026-03-01 09:21:20 +0000

    gen topo et flou

--- Mist_classes.py
+++ Mist_classes.py
@@ -1,4 +1,7 @@
 # MISTLAND : CLASSES
+import random
+
+
 class monde :
     def __init__(self) :
         self.name = "Mistland"
@@ -8,13 +11,31 @@ class monde :
 class Topographie :
     def __init__(self):
         self.cartes = []
-    def generation(self, size: int = 10) :
+    def generation_carte(self, size: int = 10) :
         self.carte = [[Zone() for _ in range(size)] for _ in range(size)]
-    def afficher(self) :
+        self.generation_altitude()
+    def flouter_altitude(self) :
+        for i in range(len(self.carte)) :
+            for j in range(len(self.carte[i])) :
+                self.carte[i][j].altitude = int((self.carte[(i-1)%len(self.carte)][j].altitude + self.carte[(i+1)%len(self.carte)][j].altitude + self.carte[i][(j-1)%len(self.carte[i])].altitude + self.carte[i][(j+1)%len(self.carte[i])].altitude) / 4)
+    def generation_altitude(self) :
         for ligne in self.carte :
             for zone in ligne :
-                print(f"Altitude : {zone.altitude}, Terrain : {zone.terrain.type}, Vegetation : {zone.vegetation.rase} et {zone.vegetation.haute}, Humidite : {zone.humidite}, Temperature : {zone.temperature}")
-            print("\n")
+                zone.altitude = random.randint(0, 9)
+    def afficher_terrain(self) :
+        for ligne in self.carte :
+            dessin_ligne = ""
+            for zone in ligne :
+                dessin_ligne += f"{zone.terrain.types[zone.terrain.type]} "
+            print(dessin_ligne)
+        print("\n")
+    def afficher_altitude(self) :
+        for ligne in self.carte :
+            dessin_ligne = ""
+            for zone in ligne :
+                dessin_ligne += f"{zone.altitude} "
+            print(dessin_ligne)
+        print("\n")
 
 class Zone :
     def __init__(self):
@@ -26,7 +47,7 @@ class Zone :
 
 class Terrain :
     def __init__(self):
-        self.types = {"roche":"0", "terre":"1", "eau":"2", "sable":"3", "glace":"4"}
+        self.types = {"roche":"#", "terre":" ", "eau":"~", "sable":".", "glace":"*"}
         self.type = list(self.types.keys())[0]
 
 class Vegetation :
@@ -36,3 +57,9 @@ class Vegetation :
         self.hautes = ["arbres", "buissons", "vignes"]
         self.haute = self.hautes[0]
 
+# Exemple d'utilisation
+test = monde()
+test.topographie.generation_carte(5)
+test.topographie.afficher_altitude()
+test.topographie.flouter_altitude()
+test.topographie.afficher_altitude()
